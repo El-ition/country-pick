1c1886514aa75d382c50f4d8841ba2cb489ffba4
hes <hesam.lfl@gmail.com>
2021-11-08 12:34:23 +0330

    fixing highdet page in sass/ first prototype

--- src/components/CountryLowDet.js
+++ src/components/CountryLowDet.js
@@ -9,11 +9,21 @@ function CountryLowDet({ data, clickedCountry }) {
     <div className="countryLowDet__container">
       <img src={data.flags.png} alt={data.name} />
       <h1>{data.name}</h1>
-      <h3>Population: {data.population.toLocaleString()}</h3>
-      <h3>region: {data.region}</h3>
-      <h3>capital: {data.capital}</h3>
-      <Link to={`/country/${data.name}`} onClick={switchTomoreDetail}>
-        Click to see More!!
+      <h3>
+        Population: <span>{data.population.toLocaleString()}</span>
+      </h3>
+      <h3>
+        region: <span>{data.region}</span>
+      </h3>
+      <h3>
+        capital: <span>{data.capital}</span>
+      </h3>
+      <Link
+        className="countryLowDet__container-link"
+        to={`/country/${data.name}`}
+        onClick={switchTomoreDetail}
+      >
+        More details...
       </Link>
     </div>
   );
--- src/pages/CountrySpec.js
+++ src/pages/CountrySpec.js
@@ -4,6 +4,7 @@ import { countries } from 'country-data';
 
 function CountrySpec({ highDetCount, clickedCountry }) {
   const history = useHistory();
+  console.log(highDetCount);
 
   // useEffect(() => {
   //   clickedCountry(highDetCount.id);
@@ -11,41 +12,63 @@ function CountrySpec({ highDetCount, clickedCountry }) {
   // console.log(highDetCount);
   // console.log(highDetCount.currencies[0].name);
   return (
-    <>
-      <div>
-        <div>
-          <button onClick={() => history.push('/')}>Back</button>
-        </div>
-        <div>
-          <img src={highDetCount.flags.svg} alt={highDetCount.name} />
-        </div>
-        <div>
-          <h1>{highDetCount.name}</h1>
-          <h3>Native Name: {highDetCount.nativeName}</h3>
-          <h3>Population: {highDetCount.population.toLocaleString()}</h3>
-          <h3>Region: {highDetCount.region}</h3>
-          <h3>SubRegion: {highDetCount.subregion}</h3>
-          <h3>Capital: {highDetCount.capital}</h3>
-          <h3>Timezones: {highDetCount?.timezones[0]}</h3>
-          <h3>
-            Currencies:
-            {highDetCount?.currencies[0]?.name}
-          </h3>
-          <div>
-            Languages:
-            {highDetCount?.languages.map((lang, i) => (
-              <h4 key={i}>{lang.name}</h4>
-            ))}
+    <div className="countrySpec">
+      <div className="countrySpec__back">
+        <button onClick={() => history.push('/')}>Back</button>
+      </div>
+      <div className="countrySpec__body">
+        <img src={highDetCount.flags.svg} alt={highDetCount.name} />
+
+        <div className="countrySpec__body-detail">
+          <div className="countrySpec__body-detail-base">
+            <div>
+              <h1>{highDetCount.name}</h1>
+            </div>
+            <div className="countrySpec__body-detail-base-divide">
+              <div className="div1">
+                <h2>
+                  Native Name: <span>{highDetCount.nativeName}</span>{' '}
+                </h2>
+                <h2>
+                  Population:{' '}
+                  <span>{highDetCount.population.toLocaleString()}</span>{' '}
+                </h2>
+                <h2>
+                  Region: <span>{highDetCount.region}</span>{' '}
+                </h2>
+                <h2>
+                  SubRegion: <span>{highDetCount.subregion}</span>{' '}
+                </h2>
+                <h2>
+                  Capital: <span>{highDetCount.capital}</span>{' '}
+                </h2>
+              </div>
+              <div className="div2">
+                <h2>
+                  Timezones: <span> {highDetCount?.timezones[0]}</span>
+                </h2>
+                <h2>
+                  Currencies:
+                  <span> {highDetCount?.currencies[0]?.name}</span>
+                </h2>
+                <h2>
+                  Languages:{' '}
+                  {highDetCount?.languages.map((lang, i) => (
+                    <span key={i}>{lang.name}, </span>
+                  ))}
+                </h2>
+              </div>
+            </div>
           </div>
-          <div>
-            Border Countries:
+          <div className="countrySpec__body-detail-neib">
+            <h2>Border Countries:</h2>
             {highDetCount?.borders?.map((bord, i) => (
               <h4 key={i}>{countries[`${bord}`].name}</h4>
             ))}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
